a4fd63c85cb242fe4a744c7d41fbec2cebe7059c
8Richard <146356679+8Richard@users.noreply.github.com>
2025-07-08 16:46:36 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-/*# Copyright (c) HashiCorp, Inc.
+# Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
 variable "region" {
@@ -6,14 +6,5 @@ variable "region" {
   default     = "us-west-1"
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
-}
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
-}
 
-*/
