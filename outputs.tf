3bbd3829bdbe184b238974905efc9bf9a3dac1f0
8Richard <146356679+8Richard@users.noreply.github.com>
2025-07-08 16:46:50 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,4 @@
-/*# Copyright (c) HashiCorp, Inc.
+# Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
-}
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
-}
-
-*/
